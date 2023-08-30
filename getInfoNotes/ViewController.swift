563777196a0b1fcf1766f2e242112fa559e41215
Liam <jacox0952@gmail.com>
2023-08-30 09:28:00 -0500

    Initial Commit

---
+++ getInfoNotes/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  getInfoNotes
+//
+//  Created by WILLIAM COX on 8/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    @IBOutlet weak var ageOutlet: UITextField!
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func buttonAction(_ sender: Any) {
+        //get text out of the text field
+        var name = textFieldOutlet.text
+        var age = Int(ageOutlet.text!)
+        if let a = age {
+            var age = a + 10
+            labelOutlet.text = "Hello, \(name!)! You are \(age) years old!"
+        } else {
+            labelOutlet.text = "you are a failure"
+        }
+
+    }
+}
+
